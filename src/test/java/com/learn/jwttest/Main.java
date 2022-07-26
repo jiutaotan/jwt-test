283d66ca02823772652f491a4508425795414c14
jiutao.tan <934852617@qq.com>
2022-07-26 13:44:06 +0800

    feat(Test): 第一次提交

---
+++ src/test/java/com/learn/jwttest/Main.java
@@ -0,0 +1,11 @@
+package com.learn.jwttest;
+
+/**
+ * @author jiutao.Tan
+ * @date 2022-07-26
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("第一次提交！");
+    }
+}
