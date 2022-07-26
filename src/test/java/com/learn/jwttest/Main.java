6922ac2895ef1f6737dec43588d0657bf7afd5e1
jiutao.tan <934852617@qq.com>
2022-07-26 13:47:06 +0800

    Revert "feat(Test): 第一次提交"
    This reverts commit a1bae24a

--- src/test/java/com/learn/jwttest/Main.java
+++ src/test/java/com/learn/jwttest/Main.java
@@ -8,6 +8,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("第一次提交！");
         System.out.println("second！");
-        System.out.println("three！");
     }
 }
